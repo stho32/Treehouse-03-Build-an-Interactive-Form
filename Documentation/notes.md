815c0fa97de51a3aa62eb9e7009df114b7c10946
Stefan Hoffmann <sthoffmann@onlinehome.de>
2017-11-04 08:04:30 +0100

    Added jquery, main.js & notes for requirement R2.

--- Documentation/notes.md
+++ Documentation/notes.md
@@ -5,5 +5,14 @@
 Added autofocus to the first text input. 
 I do not know why I should need Js for this. The requirements say nothing about explicit javascript so I guess its ok. 
 
+# (R2)
+The text field "Your job role" didn't exist yet, so I copied the one with the email-Address and 
+changed it a bit. 
+I want to use jquery, so I downloaded it and saved it into js/vendor. 
+Created new main.js.
+Linked them javascript files up in the index.html.
+Since the main.js is loaded at the end of the form file I do not need to use $(document).ready.
 
+I do not want to pollute the global namespace, so I nicely encapsulate everything in an IIFE. 
+This also gives me a nice position to "use strict".
 
---
+++ Source/js/main.js
@@ -0,0 +1,34 @@
+/**
+ * The forms javascript file. 
+ */
+
+(function() {
+    "use strict";
+
+    // implementation of requirement (R2)
+    function EnableJobRoleInteraction() {
+        function SetYourJobRoleVisibility(showIt) {
+            if (showIt) {
+                $("label[for='your-job-role']").show();
+                $("#your-job-role").show();
+                return;
+            }
+            $("label[for='your-job-role']").hide();
+            $("#your-job-role").hide();
+        }
+
+        function ShowOrHideYourJobRoleDependingOnSelection() {
+            SetYourJobRoleVisibility($("#title").val() === "other")
+        }
+
+        // I do "not really know" the default value of the drop down
+        // so I just init everything by evaluation the visibility rule.
+        ShowOrHideYourJobRoleDependingOnSelection();
+
+        // Everytime the selection of the job role changes we need to 
+        // reevaluate if the job role makes sense.
+    }
+
+
+    EnableJobRoleInteraction();
+})();
